f73da1ed3777e29576c1f0d5d29ca44d53abded4
DroidZed <aymen.dragon5@gmail.com>
2023-02-23 16:27:01 +0100

    finished BoxOffice

--- BoxOffice4SIM3/BoxOffice4SIM3/DetailsViewController.swift
+++ BoxOffice4SIM3/BoxOffice4SIM3/DetailsViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 class DetailsViewController: UIViewController {
     
@@ -24,5 +25,52 @@ class DetailsViewController: UIViewController {
     }
     
     @IBAction func addToFavsAction(_ sender: Any) {
+        let alertController = UIAlertController(title: "Alert", message: "Do you want to add this movie to your favorites?", preferredStyle: .alert)
+                
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: {_  in self.save()}))
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func save() {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+      
+      // 1
+      let managedContext =
+        appDelegate.persistentContainer.viewContext
+        
+      managedContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+      
+      // 2
+      let entity =
+        NSEntityDescription.entity(forEntityName: "Movies",
+                                   in: managedContext)!
+      
+      let movie = NSManagedObject(entity: entity,
+                                   insertInto: managedContext)
+      
+      // 3
+        movie.setValue(self.name, forKeyPath: "name")
+      
+      // 4
+      do {
+        try managedContext.save()
+        print("saved")
+          self.showToast(message: "saved", seconds: 2)
+      } catch let error as NSError {
+        print("Could not save. \(error), \(error.userInfo)")
+      }
+    }
+    
+    func showToast(message : String, seconds: Double){
+            let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+            alert.view.backgroundColor = .black
+            alert.view.alpha = 0.5
+            alert.view.layer.cornerRadius = 15
+            self.present(alert, animated: true)
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + seconds) {
+                alert.dismiss(animated: true)
+        }
     }
 }
---
+++ BoxOffice4SIM3/BoxOffice4SIM3/FavoritesViewController.swift
@@ -0,0 +1,75 @@
+//
+//  FavoritesViewController.swift
+//  BoxOffice4SIM3
+//
+//  Created by Mac-Mini-2021 on 23/2/2023.
+//
+
+import UIKit
+import CoreData
+
+class FavoritesViewController: UIViewController, UICollectionViewDataSource  {
+    
+    var movies: [String] = []
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "favCell", for: indexPath)
+        let contentView = cell.viewWithTag(0)
+        let image = contentView?.viewWithTag(1) as! UIImageView
+        let movieName = contentView?.viewWithTag(2) as! UILabel
+        
+        let name = movies[indexPath.row]
+        
+        movieName.text = name
+        image.image = UIImage(named: name)
+        
+        return cell
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func loadData() {
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+      super.viewWillAppear(animated)
+      
+      //1
+      guard let appDelegate =
+        UIApplication.shared.delegate as? AppDelegate else {
+          return
+      }
+      
+      let managedContext =
+        appDelegate.persistentContainer.viewContext
+      
+      //2
+      let fetchRequest =
+        NSFetchRequest<NSManagedObject>(entityName: "Movies")
+      
+      //3
+      do {
+          
+          for data in try managedContext.fetch(fetchRequest) {
+              movies.append(data.value(forKey: "name") as! String)
+         }
+          
+      } catch let error as NSError {
+        print("Could not fetch. \(error), \(error.userInfo)")
+      }
+    }
+
+
+
+}
